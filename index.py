7e638f06529df6757530048c99e801b5009c4efc
Shiroko <thothctm@gmail.com>
2020-05-20 17:47:46 -0400

    index try

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 from app import app
+from app import server
 from layouts import layout_fn, layout_inversiones, layout_activos, layout_extranjeros, layout_afp
 import callbacks
 
